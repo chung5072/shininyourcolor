a8a3f79d91843f5279c30ebecae86616be3906d0
chung5072 <40424414+chung5072@users.noreply.github.com>
2024-07-01 16:47:44 +0900

    fix: remove back-end files

--- backend_springboot/bottledupfeeling/src/main/kotlin/xyz/shininyourcolor/bottledupfeeling/db/repository/BFPCRepository.kt
+++
@@ -1,7 +0,0 @@
-package xyz.shininyourcolor.bottledupfeeling.db.repository
-
-import org.springframework.data.jpa.repository.JpaRepository
-import xyz.shininyourcolor.bottledupfeeling.db.entity.BottledupFeelingPageContents
-
-interface BFPCRepository : JpaRepository<BottledupFeelingPageContents, Long> {
-}
--- backend_springboot/positivetalk/src/main/kotlin/xyz/shininyourcolor/positivetalk/service/PositiveTalkService.kt
+++
@@ -1,8 +0,0 @@
-package xyz.shininyourcolor.positivetalk.service
-
-import xyz.shininyourcolor.positivetalk.dto.request.UncategorizedTalk
-
-interface PositiveTalkService {
-    // 입력한 문장에 대한 결과를 반환
-    fun categorizeContent(uncategorizedTalk: UncategorizedTalk): String
-}
